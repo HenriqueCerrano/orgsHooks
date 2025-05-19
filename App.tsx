2c6187f5eacc5fc5aa90dfb43d317f007a02d4e3
HenriqueCerrano <henrique.cerrano@gmail.com>
2025-05-19 10:52:35 -0300

    feat: add logo image and implement Top and Home UI screens

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
 
 import Home from './src/screens/Home';
 
